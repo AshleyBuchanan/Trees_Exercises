e77257f51dc68d2411ff16c5cb6cb2e64be6c318
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-03-31 12:08:38 -0700

    working through recursion. I think it makes sense.

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -45,32 +45,5 @@ class Tree {
     }
 }
 
-// // build small tree
-// let nSmall = new TreeNode(1);
-// let nSmall2 = new TreeNode(2);
-// nSmall.children.push(nSmall2);
-// smallTree = new Tree(nSmall);
-
-// // build large tree
-// let n = new TreeNode(1);
-// let n2 = new TreeNode(2);
-// let n3 = new TreeNode(3);
-// let n4 = new TreeNode(4);
-// let n5 = new TreeNode(5);
-// let n6 = new TreeNode(6);
-// let n7 = new TreeNode(7);
-// let n8 = new TreeNode(8);
-
-// n.children = [n2, n3, n4];
-
-// n4.children.push(n5, n6);
-// n6.children.push(n7);
-// n7.children.push(n8);
-
-// largeTree = new Tree(n);
-
-// emptyTree = new Tree();
-// console.log(emptyTree.sumValues());
-// console.log(largeTree.sumValues())
 
 module.exports = { Tree, TreeNode };
